d41d5a3f41e16e34de0ea0460082b0f02a2b2a3f
Di Ruan <ruandi88@gmail.com>
2015-11-29 14:16:40 -0500

    get artist info

--- facebook_profile/profile.js
+++ facebook_profile/profile.js
@@ -1,7 +1,8 @@
-var id = "43ZHCT0cAZBISjO8DG9PnE";
+var artist_id = "43ZHCT0cAZBISjO8DG9PnE";
+var api_key = "XUYQDKM596JS3A6GC";
 
-function getTopTracks(id) {
-	var link = 'https://api.spotify.com/v1/artists/' + id + '/top-tracks';
+function getTopTracks(artist_id) {
+	var link = 'https://api.spotify.com/v1/artists/' + artist_id + '/top-tracks';
 	$.ajax({
         url: link,
         data: { 
@@ -21,4 +22,49 @@ function getTopTracks(id) {
     });
 }
 
-getTopTracks(id);
+function getArtistNews(artist_id) {
+	var link = 'http://developer.echonest.com/api/v4/artist/news?';
+	$.ajax({
+        url: link,
+        data: { 
+            "api_key": api_key,
+            "id": 'spotify:artist:' + artist_id,
+            "format": 'json'
+        },
+        cache: true,
+        type: "GET",
+        success: function(response) {
+            console.log(response);
+            if(response && response.response && response.response.news) {
+            	var news = response.response.news;
+            	for(var i=0; i<news.length; i++) {
+            		console.log(news[i].name);
+            	}
+            }
+        }
+    });
+}
+
+function getArtistInfo(artist_id) {
+	var link = 'http://developer.echonest.com/api/v4/artist/profile?';
+	$.ajax({
+        url: link,
+        data: { 
+            "api_key": api_key,
+            "id": 'spotify:artist:' + artist_id,
+            "format": 'json',
+            "bucket": 'artist_location'
+        },
+        cache: true,
+        type: "GET",
+        success: function(response) {
+            console.log(response);
+            if(response) {
+            
+            }
+        }
+    });
+}
+
+getTopTracks(artist_id);
+getArtist(artist_id);
