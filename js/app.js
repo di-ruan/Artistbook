9e2bf3076ab3f131f6f92c3b4bc8ba6f76fda6c1
Nicolas Mesa <nicolasmesa@gmail.com>
2015-11-29 13:49:01 -0500

    Fixing minor bugs

--- js/app.js
+++ js/app.js
@@ -1,5 +1,27 @@
+/**
+ * Global var containing some config.
+ */
+var sa = {
+    config: {
+        tabNamesMap: {
+            timeline: 'profile-timeline-tab',
+            about: 'profile-about-tab',
+            photos: 'profile-photos-tab',
+            songs: 'profile-songs-tab',
+            following: 'following-tab',
+            history: 'history-tab',
+            searchresults: 'search-results-tab'
+        }
+    }
+};
+
+/**
+ * Initializes the events
+ */
 $(document).ready(function(){
    initTabs();
+   initPicModal();
+   initProfilePic();
 });
 
 /**
@@ -26,7 +48,123 @@ function initTabs() {
     });   
 }
 
-function selectTab(tabString) {
-    
+/**
+ * Selects a tab by id
+ */
+function selectTab(tabId) {
+    $('#' + tabId).trigger('click');
 }
 
+/**
+ * Selects a tab by tab name (see config)
+ */
+function selectTabByName(tabName){
+    if(!tabName || !tabName.toLowerCase) {
+        console.log('tab name required and has to be a string');
+        return;
+    }
+    
+    tabName = tabName.toLowerCase();
+    
+    if (sa.config.tabNamesMap[tabName]) {
+        selectTab(sa.config.tabNamesMap[tabName]);
+    } else {
+        console.log("Tab name " + tabName + ' not found in tabNamesMap', sa.config.tabNamesMap);
+    }
+}
+
+/**
+ * Selects a profile tab
+ */
+function selectProfileTab(tabName) {
+    selectTabByName(tabName);
+}
+
+/**
+ * Selects a search tab
+ */
+function selectSearchTab(tabName) {
+    selectTabByName(tabName);
+}
+
+/**
+ * Initializes the pic modal events
+ */
+function initPicModal() {
+    var imageModal = $('#image-modal'),
+            x = imageModal.find('.x-x'),
+            img = imageModal.find('img');
+    
+    img.click(function(e){
+        e.preventDefault();
+        return false;
+    })
+    
+    imageModal.click(function(e){
+        e.preventDefault();
+        hidePicModal();
+    });
+    
+    x.click(function(e){
+        e.preventDefault();
+        hidePicModal();
+    })
+}
+
+/**
+ * Hides the pic modal
+ */
+function hidePicModal() {
+    var imageModal = $('#image-modal');      
+    
+    imageModal.removeClass('x-active');
+}
+
+/**
+ * Displays the pic modal and changes the source of the pick to the src passed
+ * as a param
+ */
+function showPicModal(picSrc) {
+    var imageModal = $('#image-modal'),
+            img = imageModal.find('img');
+    
+    imageModal.addClass('x-active');
+    img.attr('src', picSrc);
+}
+
+/**
+ * Initializes the event for profile pic
+ */
+function initProfilePic(){
+    var profilePic = $('#profile-picture');    
+    addModalPicEvent(profilePic);
+}
+
+/**
+ * Adds click event to display modal pic
+ */
+function addModalPicEvent(pic) {
+    pic.click(function(e){
+       e.preventDefault();
+       
+       var picSrc = pic.attr('src');
+       
+       if (picSrc) {
+           showPicModal(picSrc);
+       }
+    });
+}
+
+/**
+ * Sets the url to the iframe containing the follow widget
+ */
+function setFollowWidgetUrl(url) {
+    $('iframe#follow-widget').attr('src', url);
+}
+
+/**
+ * Sets the url for the iframe containing the play widget
+ */
+function setPlayWidgetUrl(url) {
+    $('iframe#play-widget').attr('src', url);
+}
