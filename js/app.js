e6da5b3881d674e00ea3353a44fe51a377e50715
Nicolas Mesa <nicolasmesa@gmail.com>
2015-11-29 11:26:34 -0500

    Finishing tabs on the left

--- js/app.js
+++ js/app.js
@@ -1,3 +1,32 @@
 $(document).ready(function(){
-   console.log('ready');
+   initTabs();
 });
+
+/**
+ * Initializes the tabs
+ */
+function initTabs() {
+    var tabs = $('.x-tab');
+    
+    tabs.click(function(){
+       var tab = $(this),
+            contentId = '#' + tab.attr('data-content'),            
+            content = $(contentId),
+            tabsList = tab.closest('.x-tabs'),
+            contentHolderId = '#' + tabsList.attr('data-content-holder'),
+            contentHolder = $(contentHolderId),
+            contents = contentHolder.find('.x-tab-content'),
+            tabs = tabsList.find('.x-tab');
+       
+       tabs.removeClass('x-active');       
+       tab.addClass('x-active');
+       
+       contents.removeClass('x-active');
+       content.addClass('x-active');
+    });   
+}
+
+function selectTab(tabString) {
+    
+}
+
